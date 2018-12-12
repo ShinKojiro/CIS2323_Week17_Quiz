e86212863ff73725a1b0ac9acc32e705ec00c060
James Hall <42621442+ShinKojiro@users.noreply.github.com>
2018-12-12 01:00:32 -0600

    Finish

--- BookArray.java
+++ BookArray.java
@@ -1,25 +1,46 @@
-import java.util.*
+import java.util.*;
 
 public class BookArray{
 	public static void main(String[] args){
-		/* public String titleChoice;
-		public int bookTypeChoice;
-		public int bookCount = 0;
-		String books[] = new String[10];
-		Scanner inputDevice = new Scanner;
-		
+		String titleChoice;
+		int bookTypeChoice;
+		int bookCount = 0;
+		String[] books = new String[10];
+		int[] bookType = new int[10];
+		boolean trySuccess = false;
+		Scanner inputDevice = new Scanner(System.in);
+
 		while(bookCount != 10){
 			System.out.print("Enter book name: ");
 			titleChoice = inputDevice.nextLine();
-			System.out.print("Fiction or Non-Fiction");
-			try{
-				bookTypeChoice = inputDevice.nextInt;
+			books[bookCount] = titleChoice;
+			System.out.println("1) Fiction\n2) Non-Fiction");
+			while(trySuccess == false){
+				try{
+					bookTypeChoice = inputDevice.nextInt();
+					bookType[bookCount] = bookTypeChoice;
+					inputDevice.nextLine();
+					trySuccess = true;
+				}
+				catch(InputMismatchException e){
+					System.out.println("Enter 1 or 2");
+					inputDevice.nextLine();
+				}
 			}
-			catch(inputTypeMismatchException
 			
 			bookCount ++;
-		} */
+			trySuccess = false;
+		}
+
+		inputDevice.close();
 		
-		//String books[] = ;
+		for(int i = 0; i < 10; i++){
+			if(bookType[i] == 1){
+				System.out.println(books[i] + ": Fiction");
+			}
+			else if(bookType[i] == 2){
+				System.out.println(books[i] + ": Non-Fiction");
+			}	
+		}
 	}
 }
